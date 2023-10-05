f2ab07ed5e003ec3659b56844bfdedc0d1b7c0e5
CyberViking949 <stevensmith529@gmail.com>
2023-10-05 06:00:08 -0700

    feat: updating firewall module custom ruleset

--- firewall/main.tf
+++ firewall/main.tf
@@ -1,58 +1,22 @@
-#resource "cloudflare_filter" "non_us" {
-#   zone_id    = var.zone_id
-#   description = "block non-US"
-#   expression = "ip.geoip.country ne \"US\""
-#   lifecycle {
-#      ignore_changes = [
-#       zone_id
-#    ]
-#  }
-#}
-
-#resource "cloudflare_firewall_rule" "non_us_rule" {
-#  description   = "block non-us"
-#  filter_id = cloudflare_filter.non_us.id
-#  action    = "block"
-#  priority = 1
-#  zone_id     = var.zone_id
-#  lifecycle {
-#      ignore_changes = [
-#        zone_id
-#    ]
-#  }
-#}
-
-#resource "cloudflare_filter" "block_bots" {
-#   zone_id    = var.zone_id
-#   description = "block bots"
-#   expression = "cf.client.bot"
-#   lifecycle {
-#      ignore_changes = [
-#       zone_id
-#   ]
-# }
-#}
-
-#resource "cloudflare_firewall_rule" "block_bots_rule" {
-#  description   = "block bots"
-#  filter_id = cloudflare_filter.block_bots.id
-#  action    = "block"
-#  priority = 2
-#  zone_id     = var.zone_id
-#  lifecycle {
-#      ignore_changes = [
-#        zone_id
-#}
-#}
-
 resource "cloudflare_ruleset" "custom_rulesets" {
-  for_each = local.rules
-
   zone_id   = var.zone_id
-  name      = each.value.name
-  description = each.value.description  
+  name      = "custom default rulesets"
+  description = "custom default rulesets"
   kind    = "zone"
   phase      = "http_request_firewall_custom"
+  rules {
+    description = "block bad IP's"
+    expression = "(ip.src in $blacklist)"
+    action = "block"
+    enabled = true
+    }
+  rules {
+    description = "block bots"
+    expression = "cf.client.bot"
+    action = "block"
+    enabled = true
+    }
+    for_each = local.rules
   rules {
     description = each.value.description  
     expression = each.value.expression
@@ -65,10 +29,38 @@ resource "cloudflare_ruleset" "custom_rulesets" {
       products = try(each.value.skipped_products, null)
       phases   = try(each.value.skipped_phases, null)
     }
-    }
+  }
   lifecycle {
       ignore_changes = [
         zone_id
     ]
   }
 }
+
+# resource "cloudflare_ruleset" "custom_rulesets" {
+#   for_each = local.rules
+
+#   zone_id   = var.zone_id
+#   name      = each.value.name
+#   description = each.value.description  
+#   kind    = "zone"
+#   phase      = "http_request_firewall_custom"
+#   rules {
+#     description = each.value.description  
+#     expression = each.value.expression
+#     action = (each.value.action) == null ? "block" : each.value.action
+#     enabled = true
+#     logging {
+#       enabled = try(each.value.logging, null)
+#     }
+#     action_parameters {
+#       products = try(each.value.skipped_products, null)
+#       phases   = try(each.value.skipped_phases, null)
+#     }
+#     }
+#   lifecycle {
+#       ignore_changes = [
+#         zone_id
+#     ]
+#   }
+# }
