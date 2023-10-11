f7981434b58520b1b027adb8c39490c8533af2c3
cyberviking949 <stevensmith529@gmail.com>
2023-10-11 10:49:33 -0700

    adding example for lists

---
+++ example/account/lists.tf
@@ -0,0 +1,77 @@
+module "lists" {
+  source = "github.com/cyberviking949/cloudflare-modules//lists?ref=v2.1.0"
+  account_id = "dfaoivhreiuhveriuhjnvreflaoihunverf"
+
+  account_lists = [
+    {
+      name        = "blacklist"
+      kind        = "ip"
+      description = "List for blocking bad IP's"
+      item = [
+        #  {
+        #     ip = ""
+        #     comment = ""
+        #  }
+      ]
+    },
+    {
+      name        = "nessus_scanners"
+      kind        = "ip"
+      description = "List for allowing scanners"
+      item = [
+        {
+          ip      = "34.201.223.128/25"
+          comment = "us-east-1"
+          }, {
+          ip      = "44.192.244.0/24"
+          comment = "us-east-1"
+          }, {
+          ip      = "54.175.125.192/26"
+          comment = "us-east-1"
+          }, {
+          ip      = "13.59.252.0/25"
+          comment = "us-east-2"
+          }, {
+          ip      = "18.116.198.0/24"
+          comment = "us-east-2"
+          }, {
+          ip      = "3.132.217.0/25"
+          comment = "us-east-2"
+          }, {
+          ip      = "13.56.21.128/25"
+          comment = "us-west-1"
+          }, {
+          ip      = "3.101.175.0/25"
+          comment = "us-west-1"
+          }, {
+          ip      = "54.219.188.128/26"
+          comment = "us-west-1"
+          }, {
+          ip      = "34.223.64.0/25"
+          comment = "us-west-2"
+          }, {
+          ip      = "35.82.51.128/25"
+          comment = "us-west-2"
+          }, {
+          ip      = "35.86.126.0/24"
+          comment = "us-west-2"
+          }, {
+          ip      = "44.242.181.128/25"
+          comment = "us-west-2"
+        }
+      ]
+    }
+  ]
+
+  teams_lists = [
+    {
+      name        = "allowed_domains_new"
+      type        = "DOMAIN"
+      description = "list of domains allowed through filtering"
+      items = [
+        "abcd.com",
+        "efgh.com"
+      ]
+    }
+  ]
+}
