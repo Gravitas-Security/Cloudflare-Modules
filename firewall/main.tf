dce709206a447662cdd0bfa79e31b0c3c659c85b
cyberviking949 <stevensmith529@gmail.com>
2023-10-11 10:47:16 -0700

    feat: adding module for lists

--- firewall/main.tf
+++ firewall/main.tf
@@ -1,3 +1,4 @@
+
 resource "cloudflare_ruleset" "custom_rulesets" {
   zone_id     = var.zone_id
   name        = "custom default rulesets"
@@ -36,31 +37,3 @@ resource "cloudflare_ruleset" "custom_rulesets" {
     ]
   }
 }
-
-# resource "cloudflare_ruleset" "custom_rulesets" {
-#   for_each = local.rules
-
-#   zone_id   = var.zone_id
-#   name      = each.value.name
-#   description = each.value.description  
-#   kind    = "zone"
-#   phase      = "http_request_firewall_custom"
-#   rules {
-#     description = each.value.description  
-#     expression = each.value.expression
-#     action = (each.value.action) == null ? "block" : each.value.action
-#     enabled = true
-#     logging {
-#       enabled = try(each.value.logging, null)
-#     }
-#     action_parameters {
-#       products = try(each.value.skipped_products, null)
-#       phases   = try(each.value.skipped_phases, null)
-#     }
-#     }
-#   lifecycle {
-#       ignore_changes = [
-#         zone_id
-#     ]
-#   }
-# }
---
+++ lists/main.tf
@@ -0,0 +1,28 @@
+resource "cloudflare_list" "lists" {
+  account_id  = var.account_id
+  for_each    = { for cf_list in var.account_lists : cf_list.name => cf_list }
+  name        = each.value.name
+  description = each.value.description
+  kind        = each.value.kind
+
+  dynamic "item" {
+    for_each = each.value.item  # Iterating over the item list
+
+    content {
+      value {
+        ip = each.value.kind == "ip" ? item.value.ip : null
+        asn = each.value.kind == "asn" ? item.value.asn : null
+      }
+      comment = item.value.comment
+    }
+  }
+}
+
+resource "cloudflare_teams_list" "teams_lists" {
+  account_id  = var.account_id
+  for_each = { for cf_list in var.teams_lists : cf_list.name => cf_list }
+  name        = each.value.name
+  type        = each.value.type
+  description = each.value.description
+  items = each.value.items
+}
